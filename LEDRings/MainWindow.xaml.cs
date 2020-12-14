7521b90ff876a76f3c4e54649df269993bedbee5
Sebastian Böder <xcg5555@inhouse.ad.in.gad.de>
2020-12-14 10:57:01 +0100

    Add project files.

---
+++ LEDRings/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LEDRings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double profibility;
+        private double availableAmount;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProfibilitySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.profibility = e.NewValue;
+        }
+
+        private void AvailableAmountSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.availableAmount = e.NewValue;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Persist(profibility, availableAmount);
+        }
+
+        private void Persist(double profibility, double availableAmount)
+        {
+            MessageBox.Show($"Persisting profibility: {profibility}, availableAmount: {availableAmount}");
+        }
+    }
+}
